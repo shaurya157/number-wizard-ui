9d5321ec61f95503a0aa268761d31b183c3a5385
Shaurya Sinha <shaurya157@gmail.com>
2017-04-21 00:37:27 +0300

    start scene done

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log("Level lod requested" + name);
+		print(name);	
+	}
+	
+	public void QuitRequest(){
+		Debug.Log ("Level quit requested");
+	}
+}
